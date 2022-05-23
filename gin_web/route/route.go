9c4a96c145934caa7a40c82d3451d7c423c461de
jujucom <jujucom@126.com>
2022-05-23 22:29:50 +0800

    gin samples

--- gin_web/route/route.go
+++ gin_web/route/route.go
@@ -14,6 +14,12 @@ func RegisterRouter(engine *gin.Engine) {
 		})
 	})
 
+	engine.GET("/multi.html", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "multi.html", gin.H{
+			"message": "who are you?",
+		})
+	})
+
 	// http://localhost:8080/hello?name=XXX
 	engine.GET("/hello", func(context *gin.Context) {
 		if query, exists := context.GetQuery("name"); exists {
@@ -106,7 +112,7 @@ func RegisterRouter(engine *gin.Engine) {
 			return
 		}
 		for _, file := range form.File["file"] {
-			err := context.SaveUploadedFile(file, "./")
+			err := context.SaveUploadedFile(file, "./"+file.Filename)
 			if err != nil {
 				context.Status(http.StatusInternalServerError)
 				println(err)
