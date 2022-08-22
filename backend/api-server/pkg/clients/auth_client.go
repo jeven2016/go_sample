4a6f06274fa383f3acf1e679b76f2f5d7bae7aab
jujucom <jujucom@126.com>
2022-08-22 20:30:06 +0800

    new component for downloading assets from nexus

--- backend/api-server/pkg/clients/auth_client.go
+++ backend/api-server/pkg/clients/auth_client.go
@@ -29,8 +29,21 @@ func (c *AuthClient) StartInit() error {
 	return nil
 }
 
+//CheckAccessToken Normal case:
+//UI calls {backend_api}/auth/login
+//Backend API redirects UI client to {keycloak}/realms/myrealm/protocol/openid-connect/auth/ with redirect URL as {backend_api}/auth/callback
+//Authentication happens and the handler at {backend_api}/auth/callback gets the authorization_code.
+//Above handler makes request to {{keycloak_url}}/realms/{{realm}}/protocol/openid-connect/token to get the access_token
+//Set-cookie and returns to UI client.
 func (c *AuthClient) CheckAccessToken(accessToken string) (*gocloak.RetrospecTokenResult, bool) {
 	cfg := c.Config
+	//You will need to use the public key for your keycloak realm, which you can either grab for every authentication request, or just cache.
+	//To get the public key, you can use the gocloak function GetCerts,
+	//and parse the keys list you get back for the signing method your keycloak server is using.
+	//You can cache this since it shouldn't really change. You should be able to validate tokens without making any network calls to the keycloak server, if you wanted.
+	//Another way to validate tokens is to send the token to the keycloak introspection endpoint, and let keycloak check the token.
+	//It just may take a little longer because it's a network call, depending on where your backend server and keycloak server are hosted.
+	//You can use the gocloak function RetrospectToken to send the token to keycloak for introspection.
 	result, err := c.Client.RetrospectToken(context.TODO(), accessToken, cfg.ClientId, cfg.ClientSecret, cfg.KeycloakRealm)
 	if err != nil {
 		c.Log.Warn("invalid access token to parse", zap.String("accessToken", accessToken), zap.Error(err))
--- backend/api-server/pkg/middleware/auth.go
+++ backend/api-server/pkg/middleware/auth.go
@@ -4,6 +4,8 @@ import (
 	"api/pkg/common"
 	"api/pkg/dto"
 	"api/pkg/global"
+	"context"
+	"github.com/Nerzal/gocloak/v11"
 	"github.com/duke-git/lancet/v2/convertor"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
@@ -33,6 +35,31 @@ func Auth(log *zap.Logger) gin.HandlerFunc {
 				log.Warn("the data isn't in json format", zap.String("result", result.String()))
 			}
 			log.Info("Valid token checked", zap.String("url", c.Request.URL.String()), zap.String("result", data))
+
+			var resourceName = "admin resource"
+			permissions, err := global.GlobalApp.AuthClient.Client.GetUserPermissions(context.Background(), token, "jeven", gocloak.GetUserPermissionParams{
+				ResourceID: &resourceName,
+			})
+			for p := range permissions {
+				json, _ := convertor.ToJson(p)
+				log.Info("p", zap.String("json", json))
+			}
+
+			//获取用户所有的资源权限token，然后introspect
+			partyToken2, err := global.GlobalApp.AuthClient.Client.GetRequestingPartyToken(context.Background(), token, "jeven", gocloak.RequestingPartyTokenOptions{
+				Audience: gocloak.StringP("api-server"),
+			})
+
+			partyPermissions, err := global.GlobalApp.AuthClient.Client.
+				GetRequestingPartyPermissions(context.Background(), partyToken2.AccessToken, "jeven",
+					gocloak.RequestingPartyTokenOptions{})
+
+			for _, client := range *partyPermissions {
+				var str = client.String()
+				log.Info("per", zap.String("str", str))
+			}
+
+			log.Info("partyToken2", zap.String("partyToken2", partyToken2.IDToken))
 			return
 		}
 
