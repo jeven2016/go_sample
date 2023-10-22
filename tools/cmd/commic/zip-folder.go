f3e491e02a75364015748d9aa4523bb86050d446
jeven2016 <jujucom@126.com>
2023-10-22 11:31:04 +0800

    zip directory

--- tools/cmd/commic/zip-folder.go
+++ tools/cmd/commic/zip-folder.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/duke-git/lancet/v2/fileutil"
+	"github.com/duke-git/lancet/v2/slice"
 	flag "github.com/spf13/pflag"
 	"go.uber.org/atomic"
 	"log"
@@ -31,10 +32,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	runtime.GOMAXPROCS(3)
-	var amount = atomic.NewInt32(int32(len(dirs)))
 
+	dirs = slice.Filter(dirs, func(index int, item os.DirEntry) bool {
+		return item.IsDir()
+	})
+
+	var amount = atomic.NewInt32(int32(len(dirs)))
 	for _, d := range dirs {
 		asyncHandle(d, folder, amount)
 	}
